8853a202b1dcb895e06b93c4e17375b7c7d4129d
Ivan Rogov <xlazex@gmail.com>
2019-01-10 12:33:25 +0300

    Добавил индексы для нужных мне столбцов

--- index.sh
+++ index.sh
@@ -1,13 +1,15 @@
 
 DB=$1
+SCHEMA=$2
+OPTIONS=$3
 
 echo '_______________________НАЧИНАЮ ИМПОРТ ИЗ ФАЙЛОВ__________________'
-bash import.sh $DB
+bash import.sh $DB $SCHEMA $OPTIONS
 
 echo '_______________________НАЧИНАЮ ОБНОВЛЕНИЕ СХЕМЫ__________________'
-psql -f update_schema.sql -d $DB
+PGOPTIONS=--search_path=$SCHEMA psql -f update_schema.sql -d $DB $OPTIONS
 
 echo '_______________________НАЧИНАЮ СОЗДАНИЕ ИНДЕКСОВ_________________'
-psql -f indexes.sql -d $DB
+PGOPTIONS=--search_path=$SCHEMA psql -f indexes.sql -d $DB $OPTIONS
 
 echo '_______________________ _________ГОТОВО________ _________________'
